543da714d06dee5d5109deb432f9a935ac93c72e
vanister <vanister@users.noreply.github.com>
2019-12-01 10:09:01 -0800

    Updated init-db script with vars.

--- database/init-db.sh
+++ database/init-db.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR="/scripts"
+UN="postgres"
+DB="inventory"
+
 # create the inventory db
-psql -U postgres -f /scripts/create-db.sql
+psql -U $UN -f $SCRIPT_DIR/create-db.sql
 # create the tables for the inventory db
-psql -U postgres -d inventory -f /scripts/create-tables.sql
+psql -U $UN -d $DB -f $SCRIPT_DIR/create-tables.sql
 # seed it with some data
-psql -U postgres -d inventory -f /scripts/seed-db.sql
+psql -U $UN -d $DB -f $SCRIPT_DIR/seed-db.sql
---
+++ database/scripts/seed-db.sql
@@ -0,0 +1,1 @@
+-- Seed data for the database here
