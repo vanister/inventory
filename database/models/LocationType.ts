ce3e71e0f9346195908f29b80cca0f1a62f7cf15
vanister <vanister@users.noreply.github.com>
2020-10-17 15:11:10 -0700

    Defined the LocationType model.

--- database/models/LocationType.ts
+++ database/models/LocationType.ts
@@ -1,4 +1,10 @@
-import { Model, Optional } from 'sequelize';
+import {
+  Sequelize,
+  Model,
+  DataTypes,
+  Optional
+} from 'sequelize';
+import { TableNames } from './table-names';
 
 export interface LocationTypeAtributes {
   id: number;
@@ -15,4 +21,33 @@ export class LocationType
   public id!: number;
   public name!: string;
   public description!: string | null;
+}
+
+export function initLocationType(
+  sequelize: Sequelize,
+  associations?: { [name: string]: Model }
+) {
+  LocationType.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING(1000)
+      }
+    },
+    {
+      sequelize,
+      tableName: TableNames.LocationTypes
+    }
+  );
+
+  return LocationType;
 }
