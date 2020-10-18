909153e2862758dc43dc446edf0168d5472bbc53
vanister <vanister@users.noreply.github.com>
2020-10-17 17:56:04 -0700

    Updated config

--- database/config/config.js
+++ database/config/config.js
@@ -6,14 +6,14 @@ const config = {
     username: env.INV_DB_DEV_UN || 'postgres',
     password: env.INV_DB_DEV_PW || 'postgres123',
     database: env.INV_DB_DEV || 'social_inventory_dev',
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'postgres'
   },
   test: {
     username: env.INV_DB_TEST_UN,
     password: env.INV_DB_TEST_PW,
     database: env.INV_DB_TEST || 'social_inventory_test',
-    host: env.INV_DB_TEST_HOST || '127.0.0.1',
+    host: env.INV_DB_TEST_HOST || 'localhost',
     dialect: 'postgres'
   },
   production: {
