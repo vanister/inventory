a73acba4fa317ebe1d5baa05a46b98ffc74dfb33
vanister <vanister@users.noreply.github.com>
2020-10-17 17:54:44 -0700

    Added more tablenames to enum

--- database/models/table-names.d.ts
+++ database/models/table-names.d.ts
@@ -1,3 +1,9 @@
 export const enum TableNames {
+  Buildings = 'buildings',
+  BuildingTypes = 'building_types',
+  Components = 'components',
+  ComponentTypes = 'component_types',
+  Locations = 'locations',
   LocationTypes = 'location_types',
+  UserLocations = 'user_locations'
 }
