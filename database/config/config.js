6ff73297a872126c6ba6fe9de431fe33582bfdf8
vanister <vanister@users.noreply.github.com>
2020-11-05 09:50:02 -0800

    Added port entry to config.js

--- database/config/config.js
+++ database/config/config.js
@@ -1,12 +1,14 @@
 require('dotenv/config');
 
 const { env } = process;
+
 const config = {
   development: {
     username: env.INV_DB_DEV_UN || 'postgres',
     password: env.INV_DB_DEV_PW || 'postgres123',
     database: env.INV_DB_DEV || 'social_inventory_dev',
-    host: 'localhost',
+    host: env.INV_DB_DEV_HOST || 'localhost',
+    port: env.INV_DB_DEV_PORT || 5432,
     dialect: 'postgres'
   },
   test: {
@@ -14,6 +16,7 @@ const config = {
     password: env.INV_DB_TEST_PW,
     database: env.INV_DB_TEST || 'social_inventory_test',
     host: env.INV_DB_TEST_HOST || 'localhost',
+    port: env.INV_DB_TEST_PORT || 5432,
     dialect: 'postgres'
   },
   production: {
@@ -21,7 +24,9 @@ const config = {
     password: env.INV_DB_PW,
     database: env.INV_DB || 'social_inventory',
     host: env.INV_DB_HOST,
+    port: env.INV_DB_PORT || 5432,
     dialect: 'postgres'
   }
 };
+
 module.exports = config;
