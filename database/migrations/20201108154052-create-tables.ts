ecbbe315bc3fd934bb947972803e62f868ec077c
vanister <vanister@users.noreply.github.com>
2020-11-08 08:33:40 -0800

    Added UserProfile model and migration

--- database/migrations/20201108154052-create-tables.ts
+++ database/migrations/20201108154052-create-tables.ts
@@ -1,14 +1,26 @@
 import { QueryInterface, Sequelize } from 'sequelize';
-import { userLocationAttributes, UserLocations } from '../models/UserLocations';
+import {
+  attributes as userLocationAttributes,
+  UserLocations
+} from '../models/UserLocations';
+import {
+  attributes as userProfileAttributes,
+  UserProfile
+} from '../models/UserProfile';
 
 async function up(queryInterface: QueryInterface, Sequelize: Sequelize) {
   await queryInterface.createTable(UserLocations.tableName, {
     ...userLocationAttributes
   });
+
+  await queryInterface.createTable(UserProfile.tableName, {
+    ...userProfileAttributes
+  });
 }
 
 async function down(queryInterface: QueryInterface, Sequelize: Sequelize) {
   await queryInterface.dropTable(UserLocations.tableName);
+  await queryInterface.dropTable(UserProfile.tableName);
 }
 
 export { up, down };
