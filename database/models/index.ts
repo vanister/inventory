94e5bd1459f265a82b2496014d19934f0ec2eb76
vanister <vanister@users.noreply.github.com>
2020-11-08 08:40:15 -0800

    Added UserProfile and UserLocations exports

--- database/models/index.ts
+++ database/models/index.ts
@@ -1,29 +1,37 @@
 import 'dotenv/config';
 
 import { Sequelize } from 'sequelize';
+
 import { initialize as initBuildingType } from './BuildingType';
 import { initialize as initLocationType } from './LocationType';
 import { initialize as initComponentType } from './ComponentType';
+import { initialize as initUserLocations } from './UserLocations';
+import { initialize as initUserProfile } from './UserProfile';
 
-const { INV_DB, INV_DB_UN, INV_DB_PW, INV_DB_HOST } = process.env;
+const { INV_DB, INV_DB_UN, INV_DB_PW, INV_DB_HOST, INV_DB_PORT } = process.env;
 
 const dbcontext = new Sequelize({
   dialect: 'postgres',
   database: INV_DB,
   username: INV_DB_UN,
   password: INV_DB_PW,
-  host: INV_DB_HOST
+  host: INV_DB_HOST,
+  port: +INV_DB_PORT
 });
 
 // list models here
 const BuildingType = initBuildingType(dbcontext);
 const ComponentType = initComponentType(dbcontext);
 const LocationType = initLocationType(dbcontext);
+const UserLocations = initUserLocations(dbcontext);
+const UserProfile = initUserProfile(dbcontext);
 
 export {
   dbcontext,
   // models
   BuildingType,
   ComponentType,
-  LocationType
+  LocationType,
+  UserLocations,
+  UserProfile
 };
