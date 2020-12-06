fd64c9c326589d92867e684c1649bf53eb3451d5
vanister <vanister@users.noreply.github.com>
2020-12-06 08:34:12 -0800

    Added location-type migration

---
+++ database/migrations/20201206162906-create-location-type.ts
@@ -0,0 +1,29 @@
+import { DataTypes, QueryInterface, Sequelize } from 'sequelize';
+import { LocationType } from '../models';
+
+// Migrations are independent of models
+
+export async function up(queryInterface: QueryInterface, Sequelize: Sequelize) {
+  queryInterface.createTable(LocationType.tableName, {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING(1000)
+    }
+  });
+}
+
+export async function down(
+  queryInterface: QueryInterface,
+  Sequelize: Sequelize
+) {
+  queryInterface.dropTable(LocationType.tableName);
+}
