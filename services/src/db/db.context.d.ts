94422f7549353cc8a8e0a3cd4cf791009fc51245
vanister <vanister@users.noreply.github.com>
2020-12-22 14:12:32 -0800

    Updated jsdocs

--- services/src/db/db.context.d.ts
+++ services/src/db/db.context.d.ts
@@ -5,11 +5,11 @@ export interface DbContext {
   /** True, if the underlying connection is closed. False, otherwise. */
   connectionClosed: boolean;
 
-  /** Closes the db connection. */
+  /** Close the db connection. */
   close(): Promise<void>;
 
   /**
-   * Runs a query against the database.
+   * Run a query against the database.
    * @param sql the sql query.
    * @param params Optional: the sql query params.
    * @returns An array of result rows from the query.
@@ -17,7 +17,7 @@ export interface DbContext {
   query<T>(sql: string, ...params: any[]): Promise<T[]>;
 
   /**
-   * Runs a query against the database without return any data.
+   * Run a query against the database without return any data.
    * @param sql the sql query.
    * @param params Optional: the sql query params.
    */
